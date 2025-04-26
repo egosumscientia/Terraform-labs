27cb024898fef88fdc9cb131680774cc2716b170
egosumscientia <egosumscientia@gmail.com>
2025-04-25 19:10:43 -0500

    update

--- Lab07/main.tf
+++ Lab07/main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "azurerm" {
     features {}
-    subscription_id = "5f55b5a1-09ef-4b87-b02d-30e1a8347b3d"
-    tenant_id       = "093c684c-622c-4a8c-9180-47802a4d322a"
+    subscription_id = "xyz"
+    tenant_id       = "abc"
 }
 
 # =============================
